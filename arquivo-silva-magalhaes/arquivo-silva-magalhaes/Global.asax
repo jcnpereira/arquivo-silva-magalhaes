973ecd6e715cecd8f1d4e5bb51b410c2d1f11cdb
André Carvalho <afecarvalho@gmail.com>
2014-03-17 15:09:33 +0000

    Criação do projeto

---
+++ arquivo-silva-magalhaes/arquivo-silva-magalhaes/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="arquivo_silva_magalhaes.MvcApplication" Language="C#" %>
---
+++ arquivo-silva-magalhaes/arquivo-silva-magalhaes/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(arquivo_silva_magalhaes.Startup))]
+namespace arquivo_silva_magalhaes
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
