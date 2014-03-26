c5d215131e19f0a45cedf610d038196cfb8f7110
André Carvalho <afecarvalho@gmail.com>
2014-03-26 18:13:29 +0000

    Correção da asneira que fiz no merge do Diogo.

--- arquivo-silva-magalhaes/arquivo-silva-magalhaes/Models/SiteModels/NewsItem.cs
+++ arquivo-silva-magalhaes/arquivo-silva-magalhaes/Models/SiteModels/NewsItem.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,36 +7,16 @@ namespace ArquivoSilvaMagalhaes.Models.SiteModels
 {
     public class NewsItem
     {
-
-        public NewsItem()
-        {
-            Links = new HashSet<ReferencedLink>();
-            ReferencedNewsItems = new HashSet<NewsItem>();
-            AttachedDocuments = new HashSet<DocumentAttachment>();
-        }
-
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Subtitle { get; set; }
-        public string Heading { get; set; }
-
-        [Required]
-        public string TextContent { get; set; }
+       
         public DateTime PublishDate { get; set; }
-
-        
         public DateTime ExpiryDate { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [DefaultValue(false)]
         public bool HideAfterExpiry { get; set; }
         public DateTime CreationDate { get; set; }
         public DateTime LastModificationDate { get; set; }
 
         public virtual ICollection<ReferencedLink> Links { get; set; }
         public virtual ICollection<NewsItem> ReferencedNewsItems { get; set; }
-        public virtual ICollection<DocumentAttachment> AttachedDocuments { get; set; }
+        public int MyProperty { get; set; }
     }
 }
