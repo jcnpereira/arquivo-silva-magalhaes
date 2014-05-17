d89c1885649c6778d59a9591ddc6419be8a3eca2
André Carvalho <afecarvalho@gmail.com>
2014-05-17 10:37:36 +0100

    Adição do Angular.JS aos scripts.

--- arquivo-silva-magalhaes/arquivo-silva-magalhaes/App_Start/BundleConfig.cs
+++ arquivo-silva-magalhaes/arquivo-silva-magalhaes/App_Start/BundleConfig.cs
@@ -23,12 +23,14 @@ namespace ArquivoSilvaMagalhaes
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                        "~/Scripts/angular.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
 
-            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
-                        "~/Scripts/angular.min.js"));
+            
         }
     }
 }
