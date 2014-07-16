2f9c65f76f6127bbbfa27cd2fb4a9722afac10cc
André Carvalho <afecarvalho@gmail.com>
2014-07-16 21:40:49 +0100

    Adicionados compile-time checks para as views em Razor.

---
+++ arquivo-silva-magalhaes/arquivo-silva-magalhaes/Models/ArchiveModels/Image.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ArquivoSilvaMagalhaes.Models.ArchiveModels
+{
+    public class Image
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        public string Subject { get; set; }
+
+        public DateTime ProductionDate { get; set; }
+
+        public string Publication { get; set; }
+        public string Location { get; set; }
+
+
+
+    }
+}
