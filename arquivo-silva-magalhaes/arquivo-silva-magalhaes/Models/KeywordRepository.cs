dcb330f934b029b05cc9896a89859736d0fc9748
André Carvalho <afecarvalho@gmail.com>
2014-08-07 12:07:10 +0100

    Limpeza do código do back-office.

--- arquivo-silva-magalhaes/arquivo-silva-magalhaes/Models/KeywordRepository.cs
+++ arquivo-silva-magalhaes/arquivo-silva-magalhaes/Models/KeywordRepository.cs
@@ -1,55 +1,55 @@
-﻿using ArquivoSilvaMagalhaes.Models.ArchiveModels;
-using System;
-using System.Threading.Tasks;
+﻿//using ArquivoSilvaMagalhaes.Models.ArchiveModels;
+//using System;
+//using System.Threading.Tasks;
 
-namespace ArquivoSilvaMagalhaes.Models
-{
-    public class KeywordRepository : IGenericRepository<Keyword>
-    {
-        ArchiveDataContext _db;
+//namespace ArquivoSilvaMagalhaes.Models
+//{
+//    public class KeywordRepository : IGenericRepository<Keyword>
+//    {
+//        ArchiveDataContext _db;
 
-        public KeywordRepository() : this(new ArchiveDataContext()) { }
+//        public KeywordRepository() : this(new ArchiveDataContext()) { }
 
-        public KeywordRepository(ArchiveDataContext db)
-        {
-            _db = db;
-        }
+//        public KeywordRepository(ArchiveDataContext db)
+//        {
+//            _db = db;
+//        }
 
-        public async Task<Keyword> GetById(object[] id)
-        {
-            return await _db.Keywords.FindAsync(id);
-        }
+//        public async Task<Keyword> GetById(object[] id)
+//        {
+//            return await _db.Keywords.FindAsync(id);
+//        }
 
-        public async System.Threading.Tasks.Task<Keyword> GetTranslatedById(object[] id, string languageCode)
-        {
+//        public async System.Threading.Tasks.Task<Keyword> GetTranslatedById(object[] id, string languageCode)
+//        {
 
 
-            throw new NotImplementedException();
-        }
+//            throw new NotImplementedException();
+//        }
 
-        public async System.Threading.Tasks.Task Add(Keyword entity)
-        {
-            throw new NotImplementedException();
-        }
+//        public async System.Threading.Tasks.Task Add(Keyword entity)
+//        {
+//            throw new NotImplementedException();
+//        }
 
-        public async System.Threading.Tasks.Task Remove(Keyword entity)
-        {
-            throw new NotImplementedException();
-        }
+//        public async System.Threading.Tasks.Task Remove(Keyword entity)
+//        {
+//            throw new NotImplementedException();
+//        }
 
-        public async System.Threading.Tasks.Task RemoveTranslation(Keyword entity, string languageCode)
-        {
-            throw new NotImplementedException();
-        }
+//        public async System.Threading.Tasks.Task RemoveTranslation(Keyword entity, string languageCode)
+//        {
+//            throw new NotImplementedException();
+//        }
 
-        public async System.Threading.Tasks.Task Update(Keyword entity)
-        {
-            throw new NotImplementedException();
-        }
+//        public async System.Threading.Tasks.Task Update(Keyword entity)
+//        {
+//            throw new NotImplementedException();
+//        }
 
-        public async System.Threading.Tasks.Task UpdateTranslation(Keyword entity, string languageCode)
-        {
-            throw new NotImplementedException();
-        }
-    }
-}
+//        public async System.Threading.Tasks.Task UpdateTranslation(Keyword entity, string languageCode)
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+//}
