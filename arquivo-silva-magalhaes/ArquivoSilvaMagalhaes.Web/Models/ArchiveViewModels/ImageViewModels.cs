291e6e9a963675d6eab7a186342f5f1256972495
André Carvalho <afecarvalho@gmail.com>
2014-08-19 21:54:36 +0100

    Cleanup.

---
+++ arquivo-silva-magalhaes/ArquivoSilvaMagalhaes.Web/Models/ArchiveViewModels/ImageViewModels.cs
@@ -0,0 +1,54 @@
+﻿using ArquivoSilvaMagalhaes.Models.ArchiveModels;
+using ArquivoSilvaMagalhaes.Resources;
+using ArquivoSilvaMagalhaes.Models.Translations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArquivoSilvaMagalhaes.Models.ArchiveViewModels
+{
+
+
+    public class ImageViewModel
+    {
+        public Image Image { get; set; }
+
+        public List<SelectListItem> AvailableKeywords { get; set; }
+        public List<SelectListItem> AvailableDocuments { get; set; }
+    }
+
+    public class ImageEditViewModel
+    {
+        public Image Image { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(ValidationErrorStrings), ErrorMessageResourceName = "MustChooseAtLeastOne")]
+        [Display(ResourceType = typeof(ImageStrings), Name = "Keywords")]
+        public List<SelectListItem> AvailableKeywords { get; set; }
+
+        [Required]
+        [Display(ResourceType = typeof(ImageStrings), Name = "Document")]
+        public List<SelectListItem> AvailableDocuments { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(ValidationErrorStrings), ErrorMessageResourceName = "MustChooseAtLeastOne")]
+        public int[] KeywordIds { get; set; }
+
+        public HttpPostedFileBase ImageUpload { get; set; }
+    }
+
+    public class ImageCreateModel
+    {
+        [Display(ResourceType = typeof(DataStrings), Name = "Image__ProductionDate")]
+        [DataType(DataType.Date)]
+        public DateTime ProductionDate { get; set; }
+
+        [Required]
+        [Index(IsUnique = true)]
+        [MaxLength(50)]
+        public string ImageCode { get; set; }
+
+
+    }
+}
---
+++ arquivo-silva-magalhaes/ArquivoSilvaMagalhaes.Web/Models/ArchiveViewModels/KeywordViewModels.cs
@@ -0,0 +1,27 @@
+﻿using ArquivoSilvaMagalhaes.Models.Translations;
+using System.ComponentModel.DataAnnotations;
+namespace ArquivoSilvaMagalhaes.Models.ArchiveViewModels
+{
+    public class KeywordViewModel
+    {
+        public int Id { get; set; }
+        [Required, Display(ResourceType = typeof(KeywordStrings), Name = "Value")]
+        public string Value { get; set; }
+    }
+
+    //public class KeywordEditViewModel
+    //{
+    //    public int Id { get; set; }
+
+    //    public int KeywordId { get; set; }
+
+    //    [Required]
+    //    public string LanguageCode { get; set; }
+
+    //    [Required, Display(ResourceType = typeof(DataStrings), Name = "Keyword")]
+    //    public string Value { get; set; }
+
+    //    [NotMapped]
+    //    public IEnumerable<SelectListItem> AvailableLanguages { get; set; }
+    //}
+}
