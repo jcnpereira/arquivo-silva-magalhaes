6a029b0920a0925bc291bc2c2ff3c00be438ccc7
DiogoNeto <diogoncmc@gmail.com>
2014-08-26 16:59:52 +0100

    Overlay de imagens.

---
+++ arquivo-silva-magalhaes/ArquivoSilvaMagalhaes.Web/Scripts/frontoffice/overlay-image.js
@@ -0,0 +1,20 @@
+﻿(function ($) {
+    $('[data-afsm-show-overlay="true"]').click(function (e) {
+        var $link = $(e.target);
+        var url = $link.data('afsm-large-image-url');
+        var $target = $($link.data('afsm-target'));
+        var description = $link.data('afsm-description');
+
+        $target.find('img:first').attr('src', url);
+        $target.find('p:first').text(description);
+
+        $target.removeClass('hidden');
+
+        e.preventDefault();
+        return false;
+    });
+
+    $('[data-afsm-close-overlay]').click(function (e) {
+        $($(e.target).data('afsm-close-overlay')).addClass('hidden');
+    })
+}(window.jQuery));
