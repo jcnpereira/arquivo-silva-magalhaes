40b83a9c343c9d272e3938d62d0eb29422c5e0ca
André Carvalho <afecarvalho@gmail.com>
2014-09-09 17:34:01 +0100

    Bug fix: Correção de um bug em que os glyphicons não eram corretamente carregados em situações de debug.

--- arquivo-silva-magalhaes/ArquivoSilvaMagalhaes.Common/LanguageDefinitions.cs
+++ arquivo-silva-magalhaes/ArquivoSilvaMagalhaes.Common/LanguageDefinitions.cs
@@ -2,10 +2,7 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-using System.Threading;
-using System.Web;
 using System.Web.Mvc;
-using System.Web.Routing;
 
 namespace ArquivoSilvaMagalhaes.Common
 {
